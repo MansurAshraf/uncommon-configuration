65e928a1413346b3f1ffb706002a3c6a53cda2fb
Muhammad Ashraf <mansoor.ashraf@gmail.com>
2012-03-11 22:34:57 -0500

    added encryption test for properties configuration

--- src/test/java/com/mansoor/uncommon/configuration/PropertyConfigurationTest.java
+++ src/test/java/com/mansoor/uncommon/configuration/PropertyConfigurationTest.java
@@ -16,6 +16,8 @@
 
 package com.mansoor.uncommon.configuration;
 
+import com.mansoor.uncommon.configuration.Convertors.Converter;
+import com.mansoor.uncommon.configuration.Convertors.encryption.*;
 import com.mansoor.uncommon.configuration.exceptions.ConverterNotFoundException;
 import org.junit.Before;
 import org.junit.Test;
@@ -24,6 +26,8 @@ import java.io.File;
 import java.net.URL;
 
 import static junit.framework.Assert.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 /**
  * @author mansoor
@@ -36,6 +40,14 @@ public class PropertyConfigurationTest {
     @Before
     public void setUp() throws Exception {
         configuration = TestUtil.getPropertyConfiguration(("/testProp.properties"));
+        final KeyConfig symmetricKeyConfig = TestUtil.createSymmetricKeyConfig();
+        final KeyConfig X509Config = TestUtil.createX509KeyConfig();
+
+        final Converter<SymmetricKeyWrapper> symmetricKeyConverter = new SymmetricKeyConverter(symmetricKeyConfig);
+        final Converter<X509Wrapper> x509CertConverter = new X509CertConverter(X509Config);
+
+        configuration.getConverterRegistry().addConverter(SymmetricKeyWrapper.class, symmetricKeyConverter);
+        configuration.getConverterRegistry().addConverter(X509Wrapper.class, x509CertConverter);
     }
 
     @org.junit.Test(expected = ConverterNotFoundException.class)
@@ -83,5 +95,54 @@ public class PropertyConfigurationTest {
     public void testGetNested() throws Exception {
         final String actual = configuration.getNested(String.class, "a.b.c");
         assertEquals("abc", actual);
+    }
+
+    @Test
+    public void testEncryptedPasswordUsingSymmetricKey() throws Exception {
+        final String plainPassword = configuration.getNested(String.class, "development.password.jms");
+        assertThat(plainPassword, is(equalTo("secret jms password")));
+        configuration.setNested("development.password.jms", new SymmetricKeyWrapper(plainPassword));
+        final String encryptedPassword = configuration.getNested(String.class, "development.password.jms");
+        assertThat(encryptedPassword, is(not(equalTo(plainPassword))));
+        final SymmetricKeyWrapper decryptedPassword = configuration.getNested(SymmetricKeyWrapper.class, "development.password.jms");
+        assertThat(decryptedPassword.getPlainText(), is(equalTo(plainPassword)));
+
+        final String tempLocation = System.getProperty("java.io.tmpdir");
+        final File prop = configuration.save(tempLocation + File.separator + "symmEncrypted.properties");
+        assertThat(prop, is(notNullValue()));
+        assertTrue(prop.exists());
+    }
+
+    @Test
+    public void testEncryptedPasswordUsingX509Cert() throws Exception {
+        final String plainPassword = configuration.getNested(String.class, "development.password.database");
+        assertThat(plainPassword, is(equalTo("secret database password")));
+        configuration.setNested("development.password.database", new X509Wrapper(plainPassword));
+        final String encryptedPassword = configuration.getNested(String.class, "development.password.database");
+        assertThat(encryptedPassword, is(not(equalTo(plainPassword))));
+        final X509Wrapper decryptedPassword = configuration.getNested(X509Wrapper.class, "development.password.database");
+        assertThat(decryptedPassword.getPlainText(), is(equalTo(plainPassword)));
+
+        final String tempLocation = System.getProperty("java.io.tmpdir");
+        final File prop = configuration.save(tempLocation + File.separator + "x509Encrypted.properties");
+        assertThat(prop, is(notNullValue()));
+        assertTrue(prop.exists());
+    }
+
+    @Test
+    public void testGetX509EncryptedPasswordFromAFile() throws Exception {
+        configuration.load(this.getClass().getResource("/x509Encrypted.properties").getPath());
+        final String plainPassword = "secret database password";
+        final X509Wrapper decryptedPassword = configuration.getNested(X509Wrapper.class, "development.password.database");
+        assertThat(decryptedPassword.getPlainText(), is(equalTo(plainPassword)));
+    }
+
+    @Test
+    public void testGetSymmetricEncryptedPassword() throws Exception {
+        configuration.load(this.getClass().getResource("/symmEncrypted.properties").getPath());
+        final String plainPassword = "secret jms password";
+        final SymmetricKeyWrapper decryptedPassword = configuration.getNested(SymmetricKeyWrapper.class, "development.password.jms");
+        assertThat(decryptedPassword.getPlainText(), is(equalTo(plainPassword)));
+
     }
 }
